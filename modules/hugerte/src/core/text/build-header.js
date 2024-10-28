293760115075764ff8805590f98240eacea855a8
carlosmintfan <118076740+carlosmintfan@users.noreply.github.com>
2024-10-28 10:41:19 -0300

    Add copyright and license info to build header

--- modules/hugerte/src/core/text/build-header.js
+++ modules/hugerte/src/core/text/build-header.js
@@ -1,3 +1,6 @@
 /**
  * HugeRTE version @@version@@ (@@releaseDate@@)
+ * Copyright (c) 2022 Ephox Corporation DBA Tiny Technologies, Inc.
+ * Copyright (c) 2024 HugeRTE contributors
+ * Licensed under the MIT license (https://github.com/hugerte/hugerte/blob/main/LICENSE.TXT)
  */
