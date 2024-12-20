2525baecc528acf7536d5b5b7d28c551bc03fe1f
carlosmintfan <118076740+carlosmintfan@users.noreply.github.com>
2024-12-20 11:01:38 -0300

    Add dompurify license info to build header
    Fixes: #41

--- modules/hugerte/src/core/text/build-header.js
+++ modules/hugerte/src/core/text/build-header.js
@@ -3,4 +3,7 @@
  * Copyright (c) 2022 Ephox Corporation DBA Tiny Technologies, Inc.
  * Copyright (c) 2024 HugeRTE contributors
  * Licensed under the MIT license (https://github.com/hugerte/hugerte/blob/main/LICENSE.TXT)
+ * This file bundles the code of DOMPurify, which is dual-licensed under the Mozilla Public License v2.0 and the Apache License, Version 2.0, meaning you can use it under either one of those licenses.
+ * Copyright 2024 Dr.-Ing. Mario Heiderich, Cure53
+ * The code of DOMPurify included in this file has been modified. The latest original code of DOMPurify can be found at https://github.com/cure53/DOMPurify.
  */
