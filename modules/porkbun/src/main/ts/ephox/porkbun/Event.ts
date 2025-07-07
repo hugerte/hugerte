6487abc39e4243c70fcb11fe3d486b20fb235ffc
carlosmintfan <118076740+carlosmintfan@users.noreply.github.com>
2025-07-07 14:22:46 -0300

    porkbun: stop using katamri Arr

--- modules/porkbun/src/main/ts/ephox/porkbun/Event.ts
+++ modules/porkbun/src/main/ts/ephox/porkbun/Event.ts
@@ -1,5 +1,3 @@
-import { Arr } from '@ephox/katamari';
-
 export type EventHandler<T> = (event: T) => void;
 
 export interface Bindable<T> {
@@ -22,19 +20,19 @@ export const Event = (fields: string[]): Event => {
   };
 
   const unbind = (handler: EventHandler<any>) => {
-    // This is quite a bit slower than handlers.splice() but we hate mutation.
+    // This is quite a bit slower than handlers.splice() but we hate mutation. TODO
     // Unbind isn't used very often so it should be ok.
-    handlers = Arr.filter(handlers, (h) => {
+    handlers = handlers.filter((h) => {
       return h !== handler;
     });
   };
 
   const trigger = <T> (...args: T[]) => {
     const event: Record<string, T> = {};
-    Arr.each(fields, (name, i) => {
+    fields.forEach((name, i) => {
       event[name] = args[i];
     });
-    Arr.each(handlers, (handler) => {
+    handlers.forEach((handler) => {
       handler(event);
     });
   };
