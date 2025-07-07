d4b1b8ab71dffd9744acf9b92ef398df92b93b89
carlosmintfan <118076740+carlosmintfan@users.noreply.github.com>
2025-07-07 14:23:10 -0300

    remove unused porkbun again

--- modules/porkbun/src/main/ts/ephox/porkbun/api/Main.ts
+++ modules/porkbun/src/main/ts/ephox/porkbun/api/Main.ts
@@ -1,13 +1,11 @@
 import * as Binder from '../Binder';
 import { Bindable, Event, EventHandler } from '../Event';
 import * as Events from '../Events';
-import SourceEvent from '../SourceEvent';
 
 export {
   Binder,
   Event,
   EventHandler,
   Bindable,
-  Events,
-  SourceEvent
+  Events
 };
