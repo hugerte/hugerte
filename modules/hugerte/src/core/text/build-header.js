2e5bd83243972cb92c1ae916c7a60d87fba556c0
carlosmintfan <118076740+carlosmintfan@users.noreply.github.com>
2026-05-28 19:06:32 -0300

    chore: bump copyright year

--- modules/hugerte/src/core/text/build-header.js
+++ modules/hugerte/src/core/text/build-header.js
@@ -1,6 +1,6 @@
 /**
  * HugeRTE version @@version@@ (@@releaseDate@@)
  * Copyright (c) 2022 Ephox Corporation DBA Tiny Technologies, Inc.
- * Copyright (c) 2024 HugeRTE contributors
+ * Copyright (c) 2026 HugeRTE contributors
  * Licensed under the MIT license (https://github.com/hugerte/hugerte/blob/main/LICENSE.TXT)
  */
